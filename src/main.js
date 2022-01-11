0d33425f5984a20fc1f85f4f6982f1e68f3ee9a8
xiaoshengwpp <644619876@qq.com>
2022-01-11 16:46:17 +0800

    feat(login): login页面初始化
    1.css样式初始化2.login页面搭建

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
-
+// 初始化样式
+import './assets/css/initializeStyle.css'
 const app = createApp(App)
 installElementPlus(app)
 app.use(store).use(router).mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="zine-finish"></div>
+  <div class="main-body">
+    <div class="form-warp">
+      <h2 class="tit-h2-1">Log in</h2>
+      <el-form
+        label-position="top"
+        label-width="100px"
+        :model="netLogon"
+        style="max-width: 260px"
+      >
+        <el-form-item>
+          <label>
+            <span class="loginId">userName</span>
+          </label>
+          <el-input color="#946dd6" v-model="netLogon.name"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <label>
+            <span class="loginId">Password</span>
+          </label>
+          <el-input v-model="netLogon.password"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button color="#946dd6" class="login-btn" type="primary"
+        >Log in</el-button
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const netLogon = reactive({
+  name: '',
+  password: ''
+})
+</script>
+<style>
+body {
+  background-color: rgb(151, 109, 213);
+  min-width: 1400px;
+}
+</style>
+<style lang="scss" scoped>
+::v-deep .el-input__inner:focus {
+  border-color: #946dd6;
+}
+.zine-finish {
+  width: 830px;
+  height: 565px;
+  position: absolute;
+  left: 50%;
+  top: 48.5%;
+  transform: translate(-50%, -50%);
+  background: rgba($color: #ffffff, $alpha: 0.2);
+  border-radius: 12px;
+}
+.main-body {
+  min-width: 950px;
+  height: 565px;
+  position: absolute;
+  left: 50%;
+  top: 52%;
+  transform: translate(-50%, -50%);
+  background: #ffffff;
+  border-radius: 12px;
+}
+.form-warp {
+  position: absolute;
+  top: 20%;
+  right: 0px;
+  width: 320px;
+  // background: #ccc;
+  .tit-h2-1 {
+    color: #494e66;
+    font-weight: 600;
+    text-align: center;
+    padding-right: 60px;
+    margin-bottom: 40px;
+  }
+  .loginId {
+    color: #494e66;
+  }
+  .login-btn {
+    width: 260px;
+  }
+}
+</style>
