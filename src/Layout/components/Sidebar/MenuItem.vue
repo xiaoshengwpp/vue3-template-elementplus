1a0dd8b552b3b1bbb31945588ce859e785de4ce9
xiaoshengwpp <644619876@qq.com>
2022-01-18 13:26:13 +0800

    fix: 修复 菜单栏收缩后图标不显示问题
    修复 菜单栏收缩后图标不显示问题

--- src/Layout/components/Sidebar/MenuItem.vue
+++ src/Layout/components/Sidebar/MenuItem.vue
@@ -1,12 +1,13 @@
 <template>
-  <i v-if="icon.includes('el-icon')" class="sub-el-icon" :class="icon"></i>
-  <svg-icon v-else :icon="icon"></svg-icon>
-  <span v-show="$store.getters.sidebarOpened" class="my-tit">{{ title }}</span>
+  <!-- 这个写法饿了么新版本中好像不生效了 -->
+  <!-- <i v-if="icon.includes('el-icon')" class="sub-el-icon" :class="icon"></i> -->
+  <svg-icon :icon="icon"></svg-icon>
+  <span :class="$store.getters.sidebarOpened ? 'my-tit' : ''">{{ title }}</span>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
-defineProps({
+import { defineProps, onMounted } from 'vue'
+const props = defineProps({
   title: {
     type: String,
     required: true
@@ -15,6 +16,12 @@ defineProps({
     type: String,
     required: true
   }
+})
+// 看了文档才 猜到咋用。。。
+onMounted(() => {
+  // 没有this
+  // console.log(this)\
+  console.log(props.icon)
 })
 </script>
 
